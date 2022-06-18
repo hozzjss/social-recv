9ba7a4855a59ecf7c00c2910937b57e4792b4193
hz <13457698+hozzjss@users.noreply.github.com>
2022-06-18 15:07:39 +0200

    add external transfer functionality

--- tests/social-recovery_test.ts
+++ tests/social-recovery_test.ts
@@ -209,3 +209,42 @@ Clarinet.test({
     assertEquals(wallet2InternalBalance, types.uint(500));
   },
 });
+
+Clarinet.test({
+  name: "as a member i should be able to make an external transfer to someone else",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const deployer = accounts.get("deployer")!;
+    const wallet_1 = accounts.get("wallet_1")!;
+    const wallet_2 = accounts.get("wallet_2")!;
+
+    const contractName = deployer.address + ".social-recovery";
+
+    const wallet2InitialSTXBalance = getSTXBalance(
+      chain,
+      accounts,
+      wallet_2.address
+    );
+
+    chain.mineBlock([
+      depositTx(contractName, 1000, wallet_1.address),
+      externalTransferTx(contractName, 500, wallet_1.address, wallet_2.address),
+    ]);
+
+    const wallet1InternalBalance = getMemberBalance(
+      chain,
+      contractName,
+      wallet_1.address
+    );
+
+    assertEquals(wallet1InternalBalance, types.uint(500));
+
+    const wallet2FinalSTXBalance = getSTXBalance(
+      chain,
+      accounts,
+      wallet_2.address
+    );
+
+    // STX balance should be unchanged
+    assertEquals(wallet2FinalSTXBalance, wallet2InitialSTXBalance + 500);
+  },
+});
